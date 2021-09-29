f96ef1f1d503572118800a8b10b9236346a86486
roum02 <roum02@ewhain.net>
2021-09-29 19:46:52 +0900

    기본 레이아웃 구성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 360px;
+  height: 600px;
+  border-radius: 10px;
+  box-shadow: 0 0 25px gray;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Container>
+        <h2>TO DO LIST</h2>
+      </Container>
+    </Wrapper>
   );
 }
 
