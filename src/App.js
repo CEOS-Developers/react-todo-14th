b1b6423c5632d4b18106f4d6fb96b18d17b86cd3
[Github <[Github>
2021-09-30 14:03:03 +0900

    import clear button, modify structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import './App.css';
+import React, { useState } from "react";
+import {MdClear} from 'react-icons/md'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text: '할 일 목록 1',
+      done: false
+    },
+    {
+      id:2,
+      text: '할 일 목록 2',
+      done: false
+    }
+  ]
+  )
+  return <div>
+    <div>대기 중 (0) </div>
+  <div>아직 완료 안된 일</div>
+  <UnDoneList  todos={todos}/>
+  <div>완료된 일</div>
+
+  </div>
 }
 
+const UnDoneList = ({todos}) => {
+  return <div>{todos.map(todo => (<WriteList todo={todo} key={todo.id}/>))}</div>
+}
+
+const DoneList  = () => {
+
+}
+
+const WriteList = ({todo}) => {
+  const {id,text,done} = todo;
+  return (
+    <div className="WriteList">
+      <div>{text}</div>
+      <div className={'content ${done}'}>
+        {<MdClear />}
+      </div>
+    </div>
+  )
+}
+
+const AddList = () => {
+
+}
 export default App;
