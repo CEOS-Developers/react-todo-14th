162820eef062bd6490b0f2dbbd246913341c07f1
Hyunjae Kim <peterhyunjae@naver.com>
2021-09-30 21:05:23 +0900

    [Feat] Project 시작 및 prettier 설정

---
+++ src/components/container.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 350px;
+  height: 600px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+
+  box-shadow: 0 0 25px rgba(0, 0, 0, 0.25);
+  background: white;
+`
