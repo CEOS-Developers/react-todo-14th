6572735a8e1d7fdc774f55125aa248f7ffd12a95
Bokdol11859 <2019147551@yonsei.ac.kr>
2021-10-02 15:40:32 +0900

    combined todolist function and donelist function

--- src/components/lists/components.lists.todolist.jsx
+++ src/components/lists/components.lists.todolist.jsx
@@ -1,16 +1,63 @@
 import React, { useState } from "react";
 import "./components.lists.todolist.css";
 
-function TodoList() {
-  const [listOfToDo, setTodo] = useState(["Hello", "Hi", "hfkldsaj"]);
+function Todo({ todo, index, finishTodo, deleteTodo }) {
+  return (
+    <li>
+      <div>{todo.text}</div>
+      <div>
+        <button onClick={() => finishTodo(index)}>complete</button>
+        <button onClick={() => deleteTodo(index)}>delete</button>
+      </div>
+    </li>
+  );
+}
 
+export function TodoList() {
+  const [listOfTodo, setTodo] = useState([
+    {
+      text: "Hello",
+      complete: false,
+    },
+    {
+      text: "HelloHI",
+      complete: false,
+    },
+    {
+      text: "HIHello",
+      complete: false,
+    },
+  ]);
+
+  const addTodo = (text) => {
+    const tempTodo = [...listOfTodo, { text }];
+    setTodo(tempTodo);
+  };
+
+  const finishTodo = (index) => {
+    const tempTodo = [...listOfTodo];
+    tempTodo[index].complete = true;
+    setTodo(tempTodo);
+  };
+
+  const deleteTodo = (index) => {
+    const tempTodo = [...listOfTodo];
+    tempTodo.splice(index, 1);
+    setTodo(tempTodo);
+  };
   return (
     <div>
       <div className="todoTitle">해야할 것들</div>
       <div className="todoList">
         <ul className="todos" type="square">
-          {listOfToDo.map((item) => (
-            <li key={item}>{item}</li>
+          {listOfTodo.map((todo, index) => (
+            <Todo
+              key={index}
+              index={index}
+              todo={todo}
+              finishTodo={finishTodo}
+              deleteTodo={deleteTodo}
+            />
           ))}
         </ul>
       </div>
@@ -18,4 +65,55 @@ function TodoList() {
   );
 }
 
-export default TodoList;
+export function DoneList() {
+  const [listOfDone, setDone] = useState([
+    {
+      text: "Hello1",
+      complete: false,
+    },
+    {
+      text: "HelloHI2",
+      complete: false,
+    },
+    {
+      text: "HIHello3",
+      complete: false,
+    },
+  ]);
+
+  const addDone = (text) => {
+    const tempDone = [...listOfDone, { text }];
+    setDone(tempDone);
+  };
+
+  const finishDone = (index) => {
+    const tempDone = [...listOfDone];
+    tempDone[index].complete = true;
+    setDone(tempDone);
+  };
+
+  const deleteDone = (index) => {
+    const tempDone = [...listOfDone];
+    tempDone.splice(index, 1);
+    setDone(tempDone);
+  };
+
+  return (
+    <div>
+      <div className="doneTitle">다했다~</div>
+      <div className="doneList">
+        <ul className="dones" type="square">
+          {listOfDone.map((todo, index) => (
+            <Todo
+              key={index}
+              index={index}
+              todo={todo}
+              finishTodo={finishDone}
+              deleteTodo={deleteDone}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/components/mainpage/components.mainpage.jsx
+++ src/components/mainpage/components.mainpage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import TodoList from "../lists/components.lists.todolist";
-import DoneList from "../lists/components.lists.donelist";
+import { TodoList, DoneList } from "../lists/components.lists.todolist";
+import Form from "../form/components.form.form";
 import "./components.mainpage.css";
 
 function Mainpage() {
@@ -9,16 +9,7 @@ function Mainpage() {
       <h1>오늘 해야할 일이 뭐가 있을까 ?</h1>
       <TodoList />
       <DoneList />
-      <form className="inputForm">
-        <input
-          name="inputText"
-          className="inputText"
-          type="text"
-          placeholder="할게 뭐가 있지?"
-          autoFocus
-          onSubmit
-        />
-      </form>
+      <Form />
     </div>
   );
 }
