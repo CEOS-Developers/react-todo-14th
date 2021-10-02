33c90123a2c23bf56dcde5391ef0d24e696dddec
Bokdol11859 <2019147551@yonsei.ac.kr>
2021-10-02 17:04:51 +0900

    comments added for explanation

--- src/components/form/components.form.form.jsx
+++ src/components/form/components.form.form.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-
+//create function for input form
 function Form({ addTodo }) {
+  //initial state of input form value
   const [value, setValue] = React.useState("");
 
   const handleSubmit = (e) => {
+    //prevent reloading
     e.preventDefault();
+    //no submission if empty
     if (!value) return;
     addTodo(value);
+    //reset value
     setValue("");
   };
 
